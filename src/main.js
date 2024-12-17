4982ee0acd7860168d5d9012a9f35a18f77941d9
phane-tech <kphaneendra337@gmail.com>
2024-12-18 00:26:49 +0530

    Updated the chnges

--- src/main.js
+++ src/main.js
@@ -6,9 +6,9 @@ import { createApp } from 'vue'
 // Vuetify
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
 
 // Components
 import App from './App.vue'
@@ -17,7 +17,7 @@ import store from './store'
 
 const vuetify = createVuetify({
   components,
-  directives
+  directives,
 })
 createApp(App).use(store).use(router).use(vuetify).mount('#app')
 
