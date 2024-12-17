b3436ca2690a7d3473757002e971e558907baa1c
phane-tech <kphaneendra337@gmail.com>
2024-12-18 00:29:24 +0530

    Added different mobile components (#1)

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,64 @@
 <template>
-  <nav>
-    <v-card>
-    <v-tabs
-      v-model="tab"
-      bg-color="primary"
-    >
-      <v-tab value="one">Item One</v-tab>
-      <v-tab value="two">Item Two</v-tab>
-      <v-tab value="three">Item Three</v-tab>
-    </v-tabs>
+  <div>
+    <div> <v-select
+    v-model="value"
+    :items="items"
+    label="Select Item"
+    multiple
+  >
+    <template v-slot:selection="{ item, index }">
+      <v-chip v-if="index < 2">
+        <span>{{ item.title }}</span>
+      </v-chip>
+      <span
+        v-if="index === 2"
+        class="text-grey text-caption align-self-center"
+      >
+        (+{{ value.length - 2 }} others)
+      </span>
+    </template>
+  </v-select>
+    <v-select
+      :items="items"
+      density="compact"
+      label="Compact"
+    ></v-select>
 
-    <v-card-text>
-      <v-tabs-window v-model="tab">
-        <v-tabs-window-item value="one">
-          One
-        </v-tabs-window-item>
+    <v-select
+      :items="items"
+      density="comfortable"
+      label="Comfortable"
+    ></v-select>
 
-        <v-tabs-window-item value="two">
-          Two
-        </v-tabs-window-item>
-
-        <v-tabs-window-item value="three">
-          Three
-        </v-tabs-window-item>
-      </v-tabs-window>
-    </v-card-text>
-  </v-card>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <v-select
+      :items="items"
+      label="Default"
+    ></v-select>
+  </div>
+    <v-range-slider></v-range-slider>
+    <v-slider color="red"></v-slider>
+    <v-responsive
+    class="mx-auto"
+    max-width="344"
+  >
+    <v-text-field
+      hint="Enter your password to access this website"
+      label="Password"
+      type="input"
+      color="blue"
+      v-model="phane"
+    ></v-text-field>
+  </v-responsive>
+  </div>
 </template>
 <script>
-  export default {
-    data: () => ({
-      tab: null,
-    }),
+export default{
+  data(){
+    return{
+      items: ['foo', 'bar', 'fizz', 'buzz', 'fizzbuzz', 'foobar'],
+      value: ['foo', 'bar', 'fizz'],
+      phane: 0
+    }
   }
+}
 </script>
