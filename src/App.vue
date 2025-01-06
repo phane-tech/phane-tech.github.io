0219e34186dba65fa631cd0b831aaf86dd831e33
phane-tech <kphaneendra337@gmail.com>
2025-01-07 00:47:50 +0530

    updated the icons

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <template>
-  <div class="max-w-7xl mx-auto">
-    <h4 class="font-bold text-xl">Welcome to my page <span class="material-icons-two-tone">
-account_circle
-</span></h4>
-    <router-view/>
+  <div class="max-w-7xl mx-auto p-4">
+    <div class="bg-white p-4 rounded-xl shadow">
+      <h4 class="font-bold text-xl">Welcome to my page
+        <span class="material-icons-two-tone">account_circle</span>
+      </h4>
+      <p class="mb-0 text-sm text-gray-500 mt-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe magnam ipsam odio
+        aliquam quis dicta dolores aliquid excepturi, sunt vel ullam nesciunt quod nihil quidem unde labore inventore
+        error numquam.</p>
+    </div>
+    <router-view />
   </div>
 </template>
