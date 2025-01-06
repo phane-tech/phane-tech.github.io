f80f1f7bf89028de2c118ebf7e3dd1d6788ac8c1
phane-tech <kphaneendra337@gmail.com>
2025-01-07 00:38:59 +0530

    Added the icons

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,8 @@
 <template>
-  <router-view/>
+  <div class="max-w-7xl mx-auto">
+    <h4 class="font-bold text-xl">Welcome to my page <span class="material-icons-two-tone">
+account_circle
+</span></h4>
+    <router-view/>
+  </div>
 </template>
