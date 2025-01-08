e55159ca90df36ac18078ecb4eccd286dfc39334
phane-tech <kphaneendra337@gmail.com>
2025-01-09 00:13:21 +0530

    Added the dropdown changes

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template>
   <div class="max-w-7xl mx-auto p-4">
-    <dropdown :items="[{label:'phaneendra'}, {label:'mokshitha'}, {label:'narasimha'}, {label:'dhanalakshmi'}]"/>
+    <dropdown
+      :items="[
+        { label: 'phaneendra', icon: 'fingerprint', class: 'text-green-500' },
+        { label: 'mokshitha', icon: 'settings', class: 'text-yellow-500' },
+        { label: 'narasimha', icon: 'home', class: 'text-sky-500' },
+        { label: 'dhanalakshmi', icon: 'language', class: 'text-orange-500' },
+        { label: 'Naga Vani', class: 'text-red-500', icon: 'visibility' },
+      ]"
+    />
   </div>
 </template>
 <script>
-import dropdown from '@/components/dropdown.vue';
+import dropdown from "@/components/dropdown.vue";
 export default {
   components: { dropdown },
-  data() {
-  },
-}
+  data() {},
+};
 </script>
--- src/components/dropdown.vue
+++ src/components/dropdown.vue
@@ -1,40 +1,93 @@
 <template>
-    <Menu as="div">
-        <MenuButton>
-            <slot name="button">
-                Dropdown
+  <!-- Start of menu -->
+  <Menu as="div" :class="dropdownClass">
+    
+    <!-- start of menu button -->
+    <menu-button :class="buttonClass">
+      <slot name="button">Dropdown</slot>
+    </menu-button>
+    <!-- end of Menu button -->
+
+    <!-- start of menu -->
+    <div class="relative">
+      <!-- start of menu transition -->
+      <transition
+        enter-active-class="transition duration-100 ease-out"
+        enter-from-class="transform scale-95 opacity-0"
+        enter-to-class="transform scale-100 opacity-100"
+        leave-active-class="transition duration-75 ease-in"
+        leave-from-class="transform scale-100 opacity-100"
+        leave-to-class="transform scale-95 opacity-0"
+      >
+        <!-- start of menu items -->
+        <menu-items
+          as="ul"
+          :class="menuClass"
+          class="max-h-[400px] overflow-auto inset-[0px_auto_auto_0px] rounded-[.46rem] px-6 py-3 absolute bg-white z-[107] min-w-[200px] shadow-[0px_0px_50px_0px_rgba(82,63,105,0.15)]"
+        >
+          <!-- start of menu item -->
+          <menu-item
+            v-for="(dropdown, index) in items"
+            :key="index"
+            v-slot="{ active }"
+            as="li"
+            class="capitalize py-[9px] cursor-pointer hover:opacity-100 opacity-80"
+            :class="[itemClass, dropdown.class]"
+          >
+            <!-- start of menu item content -->
+            <slot name="item" :data="dropdown" :active="active">
+              <!-- Start of replaceble dropdown design -->
+              <div class="flex items-center gap-3">
+                <!-- start of icon -->
+                <i
+                  v-if="dropdown.icon"
+                  class="material-icons-two-tone text-2xl"
+                >
+                  {{ dropdown.icon }}
+                </i>
+                <!-- end of icon -->
+
+                <!-- start of label -->
+                <span>{{ dropdown.label }}</span>
+                <!-- end of label -->
+              </div>
+              <!-- end of replaceble dropdown design -->
             </slot>
-        </MenuButton>
-        <transition enter-active-class="transition duration-100 ease-out"
-            enter-from-class="transform scale-95 opacity-0" enter-to-class="transform scale-100 opacity-100"
-            leave-active-class="transition duration-75 ease-in" leave-from-class="transform scale-100 opacity-100"
-            leave-to-class="transform scale-95 opacity-0">
-            <MenuItems as="ul"
-                class="absolute border left-4  w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ">
-                <MenuItem v-for="(dropdown, index) in items" :key="index" v-slot="{ active }" as="li">
-                <slot name="item" :data="dropdown" :active="active">
-                    <div class="p-2 flex capitalize cursor-pointer">
-                        {{ dropdown.label }}
-                    </div>
-                </slot>
-                </MenuItem>
-            </MenuItems>
-        </transition>
-    </Menu>
+            <!-- end of menu item content -->
+          </menu-item>
+          <!-- end of menu item -->
+        </menu-items>
+        <!-- end of menu items -->
+      </transition>
+      <!-- start of menu transition -->
+    </div>
+    <!-- end of menu -->
+  </Menu>
+  <!-- end of menu -->
 </template>
 
 <script>
-import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue'
+import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/vue";
 export default {
-    name: 'DropdownMenu',
-    components: { Menu, MenuButton, MenuItems, MenuItem },
-    props: {
-        items: {
-            type: Array,
-            default: () => {
-                return [];
-            }
-        }
-    }
-}
+  // Added name to avoid multi name error
+  name: "dropdownMenu",
+  components: { Menu, MenuButton, MenuItems, MenuItem },
+  props: {
+    // Add class to the menu
+    dropdownClass: String,
+    // Add class to the button
+    buttonClass: String,
+    // Add Class to the menu
+    menuClass: String,
+    // Add class to the dropdown items
+    itemClass: String,
+    // Used to loop the menu items
+    items: {
+      type: Array,
+      default: () => [
+        { label: "phaneendra", icon: "fingerprint", class: "text-green-500" },
+      ],
+    },
+  },
+};
 </script>
