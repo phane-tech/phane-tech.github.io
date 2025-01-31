3b75929363a2674eb6a3ef3196394708c520a76e
phane-tech <kphaneendra337@gmail.com>
2025-01-31 18:30:40 +0000

    New build was released

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import "../public/main.css";
-import 'simple-notify/dist/simple-notify.css'
-
-createApp(App).use(store).use(router).mount('#app')
